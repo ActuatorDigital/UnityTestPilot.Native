c611e7f87219fb40063aa84305d74d977f94f541
Sebastiaan Fehr <seb@animaginedreality.com>
2020-04-16 11:09:14 +1000

    Added UnityDriverNative to save users composing UnitDriver themselves.

---
+++ Runtime/UnityDriverNative.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) AIR Pty Ltd. All rights reserved.
+
+using AIR.UnityTestPilot.Agents;
+
+namespace AIR.UnityTestPilot.Drivers
+{
+    public class UnityDriverNative : UnityDriver
+    {
+        public UnityDriverNative()
+            : base(new NativeUnityDriverAgent()) { }
+    }
+}
